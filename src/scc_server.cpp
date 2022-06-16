2dcc4a2fcc165ed12996dc6ffb9d7c00d3b45786
francisco <fsn9.astro@gmail.com>
2022-06-16 22:10:38 +0100

    Some services already working. A first version of the server is ready.

---
+++ src/scc_server.cpp
@@ -0,0 +1,118 @@
+#include "ros/ros.h"
+#include "scc_atlantis_ros1/SetMode.h"
+#include "scc_atlantis_ros1/Arm.h"
+#include "scc_atlantis_ros1/Takeoff.h"
+#include "mavros_msgs/SetMode.h"
+#include "mavros_msgs/CommandBool.h"
+#include <geometry_msgs/PoseStamped.h>
+
+class CmdsServer
+{
+    public:
+        CmdsServer(std::shared_ptr<ros::NodeHandle> nh)
+        {
+            // Srvs, Clients
+            set_mode_srv_ = nh->advertiseService("/scc/set_mode", &CmdsServer::set_mode_cb, this);
+            arm_srv_ = nh->advertiseService("/scc/arm", &CmdsServer::arm_cb, this);
+            takeoff_srv_ = nh->advertiseService("/scc_atlantis_ros1/takeoff", &CmdsServer::takeoff_cb, this);
+            set_mode_client_ = nh->serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+            arm_client_ = nh->serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
+
+            // Pubs
+            takeoff_pub_ = nh->advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+
+            // Saving own copy of node handle
+            nh_ = nh;
+
+            // Some parameters
+            max_altitude_ = 10;
+        }
+        bool set_mode_cb(scc_atlantis_ros1::SetMode::Request &req, scc_atlantis_ros1::SetMode::Response &res)
+        {
+            mavros_msgs::SetMode set_mode_srv;
+            set_mode_srv.request.custom_mode = req.mode;
+            
+            if (set_mode_client_.call(set_mode_srv))
+            {
+                ROS_INFO("%s mode was set!", req.mode.c_str());
+                return true;
+            }
+            else
+            {
+                ROS_ERROR("Failed to set %s mode", req.mode.c_str());
+                return false;
+            }
+        }
+        bool arm_cb(scc_atlantis_ros1::Arm::Request &req, scc_atlantis_ros1::Arm::Response &res)
+        {
+            mavros_msgs::CommandBool arm_srv;
+            arm_srv.request.value = true;
+            
+            if (arm_client_.call(arm_srv) && arm_srv.response.success)
+            {
+                ROS_INFO("arm order was sent!");
+                return true;
+            }
+            else
+            {
+                ROS_ERROR("Failed to order arm");
+                return false;
+            }
+        }
+        bool takeoff_cb(scc_atlantis_ros1::Takeoff::Request &req, scc_atlantis_ros1::Takeoff::Response &res)
+        {
+            if(req.altitude > max_altitude_)
+            {
+                ROS_ERROR("Altitude bigger than %d not allowed!", max_altitude_);
+                return false;
+            }
+
+            geometry_msgs::PoseStamped pose;
+            pose.pose.position.x = 0;
+            pose.pose.position.y = 0;
+            pose.pose.position.z = req.altitude;
+            takeoff_pub_.publish(pose);
+
+            //send a few setpoints before starting
+            ros::Rate rate(20.0);
+            for(int i = 100; ros::ok() && i > 0; --i){
+                takeoff_pub_.publish(pose);
+                ros::spinOnce();
+                rate.sleep();
+            }
+            
+            ROS_INFO("Takeoff order was sent!");
+            return true;
+        }
+
+    private:
+        std::shared_ptr<ros::NodeHandle> nh_;
+        // Servers
+        ros::ServiceServer set_mode_srv_;
+        ros::ServiceServer takeoff_srv_;
+        ros::ServiceServer arm_srv_;
+        // Clients
+        ros::ServiceClient set_mode_client_;
+        ros::ServiceClient arm_client_;
+        // Pubs
+        ros::Publisher takeoff_pub_;
+        
+        // Parameters
+        uint8_t max_altitude_;
+
+};
+
+int main(int argc, char **argv)
+{
+    // Initialize ros node object
+    ros::init(argc, argv, "scc_server");
+    // Node Handle will be shared between main and RTL server object
+    std::shared_ptr<ros::NodeHandle> nh = std::make_shared<ros::NodeHandle>();
+
+    // Create RTL server
+    CmdsServer cmds_server(nh);
+    ROS_INFO("Ready to provide MavLink command services: arm, takeoff, set_mode");
+    ros::spin();
+
+    return 0;
+}
